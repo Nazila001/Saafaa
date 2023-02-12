3845d5d95bc5e0146b8efdeb4ac05166aad46b90
Nazila <103484801+Nazila001@users.noreply.github.com>
2023-02-12 14:38:43 +0330

    Create loginComponents.ts

---
+++ loginComponents.ts
@@ -0,0 +1,14 @@
+import {Client, LoginState} from "./Model";
+import '//code.jquery.com/jquery-3.6.3.slim.js';
+
+export default class ClientListComponent {
+    private $el : any;
+
+    constructor(el : HTMLElement)
+    {
+        this.$el = $(el);
+    }
+    render(clients){
+        
+    }
+}
